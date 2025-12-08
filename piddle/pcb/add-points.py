f152bc96cdd13b82fef6cf30081df9884bdc2d96
Brandon Skari <brandon@skari.org>
2025-12-07 17:05:31 -0700

    Process holes and cuts

--- piddle/pcb/add-points.py
+++ piddle/pcb/add-points.py
@@ -1,51 +1,119 @@
+"""
+Script to lay out the elements of the board in a PCB. To use this:
+Create a board with COUNT (i.e. 15) elements, anywhere on the PCB, e.g.:
+- 15 LEDs
+- 15 resistors
+- 15 LED through holes
+- 30 mounting holes
+Save the file as piddle.kicad_pcb.backup, then run this. It will then create a
+15-sided board, and arrange the elements.
+"""
+
 import argparse
 import math
-import re
 
 COUNT = 15
+PART = math.radians(360 / COUNT)
 
 def main() -> None:
     """Main."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--holes", action="store_true", help="Set the position of the mounting holes")
     parser.add_argument("-l", "--length", type=float, required=True, help="The length from the center")
     parser.add_argument("-x", "--center_x", type=float, required=True, help="The x coordinate of the center")
     parser.add_argument("-y", "--center_y", type=float, required=True, help="The y coordinate of the center")
     args = parser.parse_args()
 
-    if args.holes:
-        # -l 90 -x 146.1133 -y 109.5963 seemed perfect
-        print(r"Processing holes - replacing {X\d} and {Y\d} with hole coordinates")
-        regex = re.compile(r"{X(\d+)} {Y(\d+)}")
+    with open("piddle.kicad_pcb.backup", "r") as file:
+        lines = file.readlines()
 
-        with open("piddle.kicad_pcb.backup", "r") as file:
-            lines = file.readlines()
+    hole_count = 0
+    ground_plane_count = 0
+    edge_cut_count = 0
 
-        count = 0
-        with open("piddle.kicad_pcb", "w") as file:
-            for line in lines:
-                if (match := regex.search(line)):
-                    number_x, number_y = [int(i) for i in match.groups()]
-                    if number_x != number_y:
-                        raise ValueError(f"number_x != number_x in line: {line}")
+    new_lines = []
 
-                    angle_spread = 7
-                    if number_x % 2 == 0:
-                        angle = math.radians(360 / COUNT) * number_x + math.radians(360 / COUNT) / 2 - math.radians(angle_spread)
-                    else:
-                        angle = math.radians(360 / COUNT) * number_x + math.radians(360 / COUNT) / 2 + math.radians(angle_spread)
+    # Calculate the points for edge cut and ground plane
+    polygon_points = []
+    for i in range(COUNT):
+        angle = PART * i
+        x = math.sin(angle) * args.length + args.center_x
+        y = math.cos(angle) * args.length + args.center_y
+        polygon_points.append((x, y))
 
-                    x = math.sin(angle) * args.length + args.center_x
-                    y = math.cos(angle) * args.length + args.center_y
-                    line = line.replace(match.group(), f"{x} {y}")
-                    count += 1
-                file.write(line)
-        
-        if count != 30:
-            raise ValueError(f"Only processed {count} lines, expected 30")
-                
-    else:
-        print("Need to specify something to do")
+    iterator = iter(lines)
+    line = None
+
+    def skip_lines(count: int, append: bool, assertion = None) -> None:
+        nonlocal line
+        for _ in range(count):
+            line = next(iterator)
+            if assertion is not None:
+                assert assertion(line), line
+            if append:
+                new_lines.append(line)
+
+    try:
+        while True:
+            line = next(iterator)
+            # Mounting holes
+            if '(footprint "MountingHole:MountingHole_3.2mm_M3" (layer "F.Cu")' in line:
+                new_lines.append(line)
+                skip_lines(1, True, lambda l: l.strip().startswith("(tstamp"))
+
+                skip_lines(1, False, lambda l: l.strip().startswith("(at"))
+                angle_spread = 4
+                if hole_count % 2 == 0:
+                    angle = PART * hole_count - math.radians(angle_spread)
+                else:
+                    angle = PART * hole_count + math.radians(angle_spread)
+
+                x = math.sin(angle) * (args.length - 10) + args.center_x
+                y = math.cos(angle) * (args.length - 10) + args.center_y
+                new_lines.append(f"    (at {x:0.4f} {y:0.4f})\n")
+                hole_count += 1
+
+            # Ground plane
+            elif '(zone (net 1) (net_name "GND") (layers "F&B.Cu")' in line:
+                new_lines.append(line)
+                # Skip the lines until we get to the polygon points
+                skip_lines(5, True, lambda l: "(xy" not in l)
+                # Skip the points
+                skip_lines(COUNT, False, lambda l: "(xy" in l)
+                for point in polygon_points:
+                    new_lines.append(f"        (xy {point[0]:0.4f} {point[1]:0.4f})\n")
+                    ground_plane_count += 1
+
+            # Edge cut
+            elif "(gr_poly" in line:
+                new_lines.append(line)
+                skip_lines(1, True, lambda l: "(pts" in l)
+                # Skip the points
+                skip_lines(COUNT, False, lambda l: "(xy" in l)
+                for point in polygon_points:
+                    new_lines.append(f"      (xy {point[0]:0.4f} {point[1]:0.4f})\n")
+                    edge_cut_count += 1
+
+            else:
+                new_lines.append(line)
+
+    except StopIteration:
+        pass
+
+    if hole_count != COUNT * 2:
+        raise ValueError(f"Only processed {hole_count} holes, expected {COUNT * 2}")
+    if edge_cut_count != COUNT:
+        raise ValueError(f"Only processed {edge_cut_count} edge cuts, expected {COUNT}")
+    if ground_plane_count != COUNT:
+        raise ValueError(f"Only processed {ground_plane_count} ground plane edges, expected {COUNT}")
+    if len(new_lines) != len(lines):
+        message = f"Line count differs: old {len(lines)}, new {len(new_lines)}"
+        print(message)
+        #raise ValueError(message)
+
+    print("Writing updated file")
+    with open("piddle.kicad_pcb", "w") as file:
+        for line in new_lines:
+            file.write(line)
 
 
 if __name__ == "__main__":
