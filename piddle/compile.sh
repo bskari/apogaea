5f9e6f3ec1d60fa64050ef1517ede85bbc1bd942
Brandon Skari <brandon@skari.org>
2026-02-17 16:31:03 -0700

    Put defines on the compile CLI

--- piddle/compile.sh
+++ piddle/compile.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-arduino-cli compile --fqbn esp32:esp32:esp32da piddle.ino $@ --warnings all
+arduino-cli compile \
+    --build-property 'compiler.cpp.extra_flags=-DNUMSTRIPS=15 -DNUM_LEDS_PER_STRIP=151' \
+    --fqbn esp32:esp32:esp32da piddle.ino $@ --warnings all
